3184f17e38a3481586cf0d8b21a933fbf9a862a5
Franco459 <francopistonecarp11@gmail.com>
2022-07-25 10:45:55 -0300

    Modelo de students

---
+++ app/models/students.js
@@ -0,0 +1,44 @@
+//modelado de students y sus caracteristicas
+const Sequelize = require('sequelize'); 
+
+module.exports = function(sequelize, DataTypes) {
+    return sequelize.define('students', {
+        student_ID: {
+            autoIncrement: true, 
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true
+        },
+        student_LU: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        student_Name:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        student_Surname: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        student_Phone: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        student_DOB: {
+            type: DataTypes.DATE,
+            allowNull: false
+        }
+
+}, {
+    sequelize,
+    tableName: 'students',
+    timestamps: false,
+    indexes: [{
+        name: "PRIMARY",
+        unique:  true,
+        using: "BTREE",
+        fields: [{ name: "student_ID" }]
+    },]    
+    });
+};
